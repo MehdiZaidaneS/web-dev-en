b17a0a6e8e3b5eb85b36df23e69f440648e43056
Mehdi <mehdicontacto@gmail.com>
2025-09-01 11:46:55 +0300

    Monday morning done

---
+++ week3/mon-morning/react-lists-lab1/src/App.jsx
@@ -0,0 +1,20 @@
+import Book from "./Books";
+import booksData from "./booksData";
+import "./Book.css"
+
+function App(){
+  return(
+    <div className="App">
+             <h1>Book List</h1>
+             <div className="book-list">
+              {
+                booksData.map(book => <Book key={book.id} book={book} name="Matti"/>)
+              }
+             </div>
+    </div>
+  )
+
+}
+
+
+export default App
